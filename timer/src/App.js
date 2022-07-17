ab3636ef478d2cd556e9ec35352989d1e3aaaa34
AshTorosyan <torosyanashotjs@gmail.com>
2022-07-17 15:08:34 +0400

    changes in CSS font-family

--- timer/src/App.js
+++ timer/src/App.js
@@ -2,19 +2,19 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 const App = () => {
-  const [timer,setTimer] = useState((new Date()).toLocaleTimeString())
+  const [timer,setTimer] = useState((new Date()).toLocaleTimeString());
 
   useEffect(() => {
    setTimeout(() => {
-    setTimer((new Date()).toLocaleTimeString())
-   },1000)
-  },[timer])
+    setTimer((new Date()).toLocaleTimeString());
+   },1000);
+  },[timer]);
 
   return (
     <section className='container'>
       {timer}
     </section>
-  )
+  );
 }
 
-export default App
+export default App;
